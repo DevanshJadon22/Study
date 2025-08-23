a965a6b6ed7fb12dd04f16f0e7e3bd20d86637b3
DevanshJadon22 <devanshjadon22@gmail.com>
2025-08-23 16:04:24 +0530

    m

---
+++ Studying_js/lecture-6/script.js
@@ -0,0 +1,3 @@
+console.log("hello!");
+//when ever you write an alert message or any prompt message in js file at the first line  then this js file will hault the execution of html and css file untill we pass message in prompt and click ok or either for alert we click ok.
+alert("apna college.")
